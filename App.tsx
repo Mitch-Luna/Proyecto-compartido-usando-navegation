4208ba7db33af7bf894b3fb19f1e5d37fb656e23
JohanaBaez <johanarabaez617@gmail.com>
2022-10-19 22:28:59 -0600

    Modificaciones en la vista principal

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola mundo!</Text>
+      <Text>Michell</Text>
       <StatusBar style="auto" />
     </View>
   );
