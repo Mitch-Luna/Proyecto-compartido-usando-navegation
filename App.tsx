c641d3827d9c128fdbbf70067fec307c2c93a0da
JohanaBaez <johanarabaez617@gmail.com>
2022-10-19 22:28:00 -0600

    modificaciones en la parte principal de la app

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola mundo!</Text>
+      <Text>Parte de Johana</Text>
       <StatusBar style="auto" />
     </View>
   );
