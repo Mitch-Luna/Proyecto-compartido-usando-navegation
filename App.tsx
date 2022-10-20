4724632571541103f1f8d4610906849b0b332843
JohanaBaez <johanarabaez617@gmail.com>
2022-10-19 22:32:47 -0600

    Desarrollo de la priemra vista

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Michell</Text>
+      <Text>Nuria, tu parte correspondiente</Text>
       <StatusBar style="auto" />
     </View>
   );
